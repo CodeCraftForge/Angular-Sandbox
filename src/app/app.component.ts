f2f6c31709730cf5fb51417270a8fae8d1ecd907
André Risch <development@andre-risch.de>
2024-04-30 00:27:20 +0200

    add imports and provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/shared/components/menu/menu.component';
+import { DataService } from './shared/services/data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, HttpClientModule],
+  providers: [DataService, HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
