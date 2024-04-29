896e24e8dd288467b0b880211433f95c5264791c
André Risch <development@andre-risch.de>
2024-04-29 05:52:25 +0200

    add menu and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home/home.component';
+import { ProductsComponent } from './components/product/products/products.component';
+import { UsersComponent } from './components/user/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'users', component: UsersComponent }
+];
