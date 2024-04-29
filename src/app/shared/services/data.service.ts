f2acdbc072a6f94e62bc4fd243aae498501f5257
André Risch <development@andre-risch.de>
2024-04-29 07:33:16 +0200

    implement data service

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from '../interfaces/user';
+import { Product } from '../interfaces/product';
 
-@Injectable({
+Injectable({
   providedIn: 'root'
 })
+
+interface Endpoints {
+  [key: string]: string;
+}
+
 export class DataService {
 
-  constructor() { }
+  host: string = 'localhost:3004'
+  endpoints: Endpoints = {
+    'users': '/users',
+    'products': '/products'
+  }
+
+  constructor(private http: HttpClient) { }
+
+  fetchProducts(): Observable<Product> {
+    return this.fetchData('products');
+  }
+
+  fetchUsers(): Observable<User> {
+    return this.fetchData('users');
+  }
+
+  private fetchData(endpoint: string): Observable<any> {
+    return this.http.get('//' + this.host + this.endpoints[endpoint as keyof Endpoints]);
+  }
 }
