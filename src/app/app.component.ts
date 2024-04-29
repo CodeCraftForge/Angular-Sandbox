e3f0a6f47fd0140c1244467412e105afcf3ee1da
André Risch <development@andre-risch.de>
2024-04-29 08:11:02 +0200

    restructure shared menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from './components/shared/menu/main/menu.component';
+import { MenuComponent } from './components/shared/components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
