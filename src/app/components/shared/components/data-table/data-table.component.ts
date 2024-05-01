cdc280f2e3dd0cb12e403b8fb4f734ee2573d81e
André Risch <development@andre-risch.de>
2024-05-02 00:03:42 +0200

    implement table sorting

--- src/app/components/shared/components/data-table/data-table.component.ts
+++ src/app/components/shared/components/data-table/data-table.component.ts
@@ -11,10 +11,44 @@ import { Component, Input } from '@angular/core';
 export class DataTableComponent {
   @Input() data: any[] = [];
   columnNames: string[] = [];
+  sortedColumn!: string;
+  sortDirection: 'asc' | 'desc' = 'asc';
 
   ngOnChanges() {
     if (this.data && this.data.length > 0) {
       this.columnNames = Object.keys(this.data[0]);
+      this.sortBy(this.columnNames[0]);
     }
+  }
+
+  sortBy(columnName: string) {
+    if (this.sortedColumn === columnName) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortedColumn = columnName;
+      this.sortDirection = 'asc';
+    }
+    this.sortData();
+  }
+
+  private sortData() {
+    if (this.sortedColumn && this.data) {
+      const allColumnDataAreNumbers = this.checkAllColumnDataAreNumbers();
+      this.data.sort((a, b) => {
+        let aValue = allColumnDataAreNumbers === true ? Number(a[this.sortedColumn]) : a[this.sortedColumn];
+        let bValue = allColumnDataAreNumbers === true ? Number(b[this.sortedColumn]) : b[this.sortedColumn];
+        if (aValue < bValue) {
+          return this.sortDirection === 'asc' ? -1 : 1;
+        }
+        if (aValue > bValue) {
+          return this.sortDirection === 'asc' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+  }
+
+  private checkAllColumnDataAreNumbers(): boolean {
+    return this.data.every(v => '' + Number(v[this.sortedColumn]) === v[this.sortedColumn]);
   }
 }
